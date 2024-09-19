2d4bb5b3c6d4d7ab366b3a9fa4f5cf2ba503cba5
MonsterusCow <ryan.stark26@d155.org>
2024-09-19 07:30:47 -0500

    Initial Commit

---
+++ GradesFunctionThing/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GradesFunctionThing
+//
+//  Created by RYAN STARK on 9/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GradesFunctionThingTests/GradesFunctionThingTests.swift
@@ -0,0 +1,17 @@
+//
+//  GradesFunctionThingTests.swift
+//  GradesFunctionThingTests
+//
+//  Created by RYAN STARK on 9/19/24.
+//
+
+import Testing
+@testable import GradesFunctionThing
+
+struct GradesFunctionThingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
